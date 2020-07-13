be0a603890cd867395d17235ec35a83a40a9e17c
Daniel Schindler <daniel@vcard24.de>
2020-07-13 22:51:18 +0200

    Added changes for markers

--- btplotting/figure.py
+++ btplotting/figure.py
@@ -18,6 +18,7 @@ from .utils import get_datadomain, get_source_id
 from .helper.label_resolver import datatarget2label, plotobj2label
 from .helper.bokeh import convert_color, sanitize_source_name, \
     set_cds_columns_from_df
+from .helper.marker import get_marker_info
 
 
 class HoverContainer(metaclass=bt.MetaParams):
@@ -173,108 +174,6 @@ class Figure(object):
 
     _tools = "pan,wheel_zoom,box_zoom,reset"
 
-    _mrk_fncs = {
-        # "."	m00	point
-        '.': ('dot', ["color"], {"size": 1}, {}),
-        # ","	m01	pixel
-        ',': ('dot', ["color"], {"size": 2}, {}),
-        # "o"	m02	circle
-        'o': ('circle', ["color", "size"], {}, {}),
-        # "v"	m03	triangle_down
-        'v': ('triangle', ["color", "size"],
-              {"angle": 180, "angle_units": "deg"}, {}),
-        # "^"	m04	triangle_up
-        '^': ('triangle', ["color", "size"], {}, {}),
-        # "<"	m05	triangle_left
-        '<': ('triangle', ["color", "size"],
-              {"angle": -90, "angle_units": "deg"}, {}),
-        # ">"	m06	triangle_right
-        '>': ('triangle', ["color", "size"],
-              {"angle": 90, "angle_units": "deg"}, {}),
-        # "1"	m07	tri_down
-        '1': ('y', ["color", "size"], {}, {}),
-        # "2"	m08	tri_up
-        '2': ('y', ["color", "size"],
-              {"angle": 180, "angle_units": "deg"}, {}),
-        # "3"	m09	tri_left
-        '3': ('y', ["color", "size"],
-              {"angle": -90, "angle_units": "deg"}, {}),
-        # "4"	m10	tri_right
-        '4': ('y', ["color", "size"],
-              {"angle": 90, "angle_units": "deg"}, {}),
-        # "8"	m11	octagon
-        '8': ('octagon', ["color", "size"], {}, {}),
-        # "s"	m12	square
-        's': ('square', ["color", "size"], {}, {}),
-        # "p"	m13	pentagon
-        'p': ('pentagon', ["color", "size"], {}, {}),
-        # "P"	m23	plus(filled)
-        'P': ('plus', ["color", "size"], {}, {"size": -2}),
-        # "*"	m14	star
-        '*': ('asterisk', ["color", "size"], {}, {}),
-        # "h"	m15	hexagon1
-        'h': ('hex', ["color", "size"], {}, {}),
-        # "H"	m16	hexagon2
-        'H': ('hex', ["color", "size"],
-              {"angle": 45, "angle_units": "deg"}, {}),
-        # "+"	m17	plus
-        '+': ('plus', ["color", "size"], {}, {}),
-        # "x"	m18	x
-        'x': ('x', ["color", "size"], {}, {}),
-        # "X"	m24	x(filled)
-        'X': ('x', ["color", "size"], {}, {"size": -2}),
-        # "D"	m19	diamond
-        'D': ('diamond_cross', ["color", "size"], {}, {}),
-        # "d"	m20	thin_diamond
-        'd': ('diamond', ["color", "size"], {}, {}),
-        # "|"	m21	vline
-        '|': ('vbar', ["color"], {}, {}),
-        # "_"	m22	hline
-        '_': ('hbar', ["color"], {}, {}),
-        # 0 (TICKLEFT)	m25	tickleft
-        0: ('triangle', ["color", "size"],
-            {"angle": -90, "angle_units": "deg"}, {"size": -2}),
-        # 1 (TICKRIGHT)	m26	tickright
-        1: ('triangle', ["color", "size"],
-            {"angle": 90, "angle_units": "deg"}, {"size": -2}),
-        # 2 (TICKUP)	m27	tickup
-        2: ('triangle', ["color", "size"], {}, {"size": -2}),
-        # 3 (TICKDOWN)	m28	tickdown
-        3: ('triangle', ["color", "size"],
-            {"angle": 180, "angle_units": "deg"}, {"size": -2}),
-        # 4 (CARETLEFT)	m29	caretleft
-        4: ('triangle', ["fill_color", "color", "size"],
-            {"angle": -90, "angle_units": "deg"}, {}),
-        # 5 (CARETRIGHT)	m30	caretright
-        5: ('triangle', ["fill_color", "color", "size"],
-            {"angle": 90, "angle_units": "deg"}, {}),
-        # 6 (CARETUP)	m31	caretup
-        6: ('triangle', ["fill_color", "color", "size"], {}, {}),
-        # 7 (CARETDOWN)	m32	caretdown
-        7: ('triangle', ["fill_color", "color", "size"],
-            {"angle": 180, "angle_units": "deg"}, {}),
-        # 8 (CARETLEFTBASE)	m33	caretleft(centered at base)
-        8: ('triangle', ["fill_color", "color", "size"],
-            {"angle": -90, "angle_units": "deg"}, {"x": 0.25}),
-        # 9 (CARETRIGHTBASE)	m34	caretright(centered at base)
-        9: ('triangle', ["fill_color", "color", "size"],
-            {"angle": 90, "angle_units": "deg"}, {"x": -0.25}),
-        # 10 (CARETUPBASE)	m35	caretup(centered at base)
-        10: ('triangle', ["fill_color", "color", "size"],
-             {}, {"y": -0.25}),
-        # 11 (CARETDOWNBASE)	m36	caretdown(centered at base)
-        11: ('triangle', ["fill_color", "color", "size"],
-             {"angle": 180, "angle_units": "deg"}, {"y": 0.25}),
-        # "None", " " or ""	 	nothing
-        '': ('text', ["text_color", "text_font_size", "text"],
-             {}, {}),
-        ' ': ('text', ["text_color", "text_font_size", "text"],
-              {}, {}),
-        # '$...$' text
-        '$': ('text', ["text_color", "text_font_size", "text"],
-              {}, {}),
-    }
-
     _style_mpl2bokeh = {
         '-': 'solid',
         '--': 'dashed',
@@ -422,8 +321,7 @@ class Figure(object):
                     hours=[self._scheme.axis_tickformat_hours],
                     days=[self._scheme.axis_tickformat_days],
                     months=[self._scheme.axis_tickformat_months],
-                    years=[self._scheme.axis_tickformat_years],
-                    ),
+                    years=[self._scheme.axis_tickformat_years]),
                 source=self._page_cds,
             ),
             code=formatter_code)
@@ -483,9 +381,9 @@ class Figure(object):
     def plot_data(self, data):
         source_id = get_source_id(data)
         self.cds_cols += [source_id + x for x in [
-                'open', 'high', 'low', 'close',
-                'colors_bars', 'colors_wicks',
-                'colors_outline']]
+            'open', 'high', 'low', 'close',
+            'colors_bars', 'colors_wicks',
+            'colors_outline']]
 
         title = sanitize_source_name(datatarget2label([data]))
         self._figure_append_title(title)
@@ -523,8 +421,7 @@ class Figure(object):
                 source=self.cds,
                 fill_color=source_id + 'colors_bars',
                 line_color=source_id + 'colors_outline',
-                legend_label=title,
-                )
+                legend_label=title)
 
             self._set_single_hover_renderer(renderer)
 
@@ -574,7 +471,7 @@ class Figure(object):
             source_data_axis = 'axvol'
 
             self.figure.extra_y_ranges = {source_data_axis: DataRange1d(
-                range_padding=1.0/self._scheme.volscaling,
+                range_padding=1.0 / self._scheme.volscaling,
                 start=0,
             )}
 
@@ -663,13 +560,7 @@ class Figure(object):
             kwglyphs['legend_label'] = label
 
             if marker is not None:
-                fnc_name, attrs, vals, updates = None, list(), dict(), dict()
-                if isinstance(marker, (int, float)):
-                    fnc_name, attrs, vals, updates = __class__._mrk_fncs[
-                        int(marker)]
-                if isinstance(marker, str):
-                    fnc_name, attrs, vals, updates = __class__._mrk_fncs[
-                        str(marker)[0]]
+                fnc_name, attrs, vals, updates = get_marker_info(marker)
                 if not fnc_name or not hasattr(self.figure, fnc_name):
                     # provide alternative methods for not available methods
                     if fnc_name == "y":
@@ -678,8 +569,8 @@ class Figure(object):
                         vals.update({"text": {"value": "y"}})
                     else:
                         raise Exception(
-                                "Sorry, unsupported marker:"
-                                + f" '{marker}'. Please report to GitHub.")
+                            "Sorry, unsupported marker:"
+                            + f" '{marker}'. Please report to GitHub.")
                         return
                 # set kwglyph values
                 kwglyphs['y'] = source_id
@@ -704,7 +595,8 @@ class Figure(object):
                         kwglyphs[u] = max(
                             1, kwglyphs[u] + val)
                     else:
-                        kwglyphs[u] = abs(val)
+                        raise Exception(
+                            f"{u} for {marker} is not set but needs to be set")
                 glyph_fnc = getattr(self.figure, fnc_name)
             elif method == "bar":
                 kwglyphs['bottom'] = 0
