c568f1403a3f08f9e49b94d9727801b18d1e2cf1
Daniel Schindler <daniel@vcard24.de>
2020-07-26 01:41:20 +0200

    fixed hover tooltips hide of NaN values

--- btplotting/templates/js/hover_tooltips.js
+++ btplotting/templates/js/hover_tooltips.js
@@ -4,16 +4,16 @@ var tmpl = Bokeh.require("core/util/templating");
 if (hover.defaults == undefined || hover.defaults == false) {
     hover.defaults = hover.tooltips;
 }
+var index = 0;
 if (cb_data.index.indices.length > 0) {
-    var ttips = [];
-    for (var i = 0; i < hover.defaults.length; i++) {
-        var val = tmpl.replace_placeholders(
-            hover.defaults[i][1],
-            source,
-            cb_data.index.indices[0]);
-        if (val != "NaN") {
-            ttips.push(hover.defaults[i]);
-        }
-    }
-    hover.tooltips = ttips;
+    index = cb_data.index.indices[0];
 }
+var ttips = [];
+for (var i = 0; i < hover.defaults.length; i++) {
+    var val = tmpl.replace_placeholders(
+        hover.defaults[i][1], source, index);
+    if (val != "NaN") {
+        ttips.push(hover.defaults[i]);
+    }
+}
+hover.tooltips = ttips;
