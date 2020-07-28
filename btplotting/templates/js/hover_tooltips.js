b1ca5d7534194468ed0032ed51514b437832deed
Daniel Schindler <daniel@vcard24.de>
2020-07-28 11:47:54 +0200

    hover tooltips and no indicies

--- btplotting/templates/js/hover_tooltips.js
+++ btplotting/templates/js/hover_tooltips.js
@@ -4,16 +4,28 @@ var tmpl = Bokeh.require("core/util/templating");
 if (hover.defaults == undefined || hover.defaults == false) {
     hover.defaults = hover.tooltips;
 }
-var index = 0;
+var index = -1;
 if (cb_data.index.indices.length > 0) {
     index = cb_data.index.indices[0];
-}
-var ttips = [];
-for (var i = 0; i < hover.defaults.length; i++) {
-    var val = tmpl.replace_placeholders(
-        hover.defaults[i][1], source, index);
-    if (val != "NaN") {
-        ttips.push(hover.defaults[i]);
+} else {
+    var column = source.get_column('index');
+    for (var i = 0; i < column.length; i++) {
+        if (column[i] == parseInt(cb_data.geometry.x)) {
+            index = i;
+            break;
+        }
     }
 }
-hover.tooltips = ttips;
+if (index >= 0) {
+    var ttips = [];
+    for (var i = 0; i < hover.defaults.length; i++) {
+        var val = tmpl.replace_placeholders(
+            hover.defaults[i][1], source, index);
+        if (val != "NaN") {
+            ttips.push(hover.defaults[i]);
+        }
+    }
+    hover.tooltips = ttips;
+} else {
+    hover.tooltips = hover.defaults;
+}
