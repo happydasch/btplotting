ee2cc29211b36c49e6d13c40a7cb2c5082543a62
Daniel Schindler <daniel@vcard24.de>
2020-07-30 13:03:24 +0200

    added comment about a bokeh bug

--- btplotting/templates/js/hover_tooltips.js
+++ btplotting/templates/js/hover_tooltips.js
@@ -8,6 +8,8 @@ var index = -1;
 if (cb_data.index.indices.length > 0) {
     index = cb_data.index.indices[0];
 } else {
+    // this is a workaround for a hit test bug:
+    // https://github.com/bokeh/bokeh/issues/8787
     var column = source.get_column('index');
     for (var i = 0; i < column.length; i++) {
         if (column[i] == parseInt(cb_data.geometry.x)) {
