cdae8a2ba6c51ee507de1f38b7aef236df7576f3
Daniel Schindler <daniel@vcard24.de>
2020-08-01 00:06:26 +0200

    new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='btplotting',
 
-    version='0.0.1',
+    version='0.1.0',
 
     description='Plotting package for Backtrader (Bokeh)',
 
