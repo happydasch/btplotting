b13b1ea9e2fd9272ea59f7dd184d2a31b539eb50
Daniel Schindler <daniel@vcard24.de>
2021-04-18 14:31:28 +0200

    updated setup script

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8; py-indent-offset:4 -*-
 import setuptools
+from distutils.util import convert_path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+main_ns = {}
+ver_path = convert_path('btplotting/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
 setuptools.setup(
     name='btplotting',
-
-    version='0.1.0',
-
+    version=main_ns['__version__'],
     description='Plotting package for Backtrader (Bokeh)',
-
     python_requires='>=3.6',
-
     author='happydasch',
     author_email='daniel@vcard24.de',
-
     long_description=long_description,
     long_description_content_type="text/markdown",
-
     license='GPLv3+',
     url="https://github.com/happydasch/btplotting",
     project_urls={
