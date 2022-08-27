9ff5baa61562a49697c2c24f8f6449f279c750c3
happydasch <daniel@vcard24.de>
2022-08-27 15:19:02 +0200

    wip data realigning

--- btplotting/clock.py
+++ btplotting/clock.py
@@ -43,24 +43,19 @@ class DataClockHandler:
         example 2:
             clock entries:
             10:00 10:01, 10:02, 10:03, 10:04, 10:05, 10:06, 10:07
-            10:08 10:09, 10:10, 10:11
             data entries:
-            10:00, 10:05, 10:10
+            10:00, 10:05
 
             will result in:
             index   clock   aligned data    fillgaps
             1       10:00   10:00           10:00
-            2       10:01   nan             10:00
-            3       10:02   nan             10:00
-            4       10:03   nan             10:00
-            5       10:04   nan             10:00
+            2       10:01   10:00           10:00
+            3       10:02   10:00           10:00
+            4       10:03   10:00           10:00
+            5       10:04   10:00           10:00
             6       10:05   10:05           10:05
-            7       10:06   nan             10:05
-            8       10:07   nan             10:05
-            9       10:08   nan             10:05
-            10      10:09   nan             10:05
-            11      10:10   10:10           10:10
-            12      10:11   nan             10:10
+            7       10:06   10:05           10:05
+            8       10:07   10:05           10:05
     '''
 
     def __init__(self, strategy, dataname=False):
@@ -76,6 +71,8 @@ class DataClockHandler:
         offset = 0
         idx = len(self._clk.array) - 1
         while True:
+            if idx < 0:
+                break
             val = self._clk.array[idx - offset]
             if val == val:
                 break
@@ -99,44 +96,99 @@ class DataClockHandler:
         '''
         res = []
         last_idx = -1 if not len(slicedata['float']) else 0
-        last_val = None
+        last_val = float('nan')
+        # loop through timestamps of curent clock as float values
         dtlist = self.get_dt_list(startidx, endidx, asfloat=True)
         for i in range(0, len(dtlist)):
-            aval = float('nan')
-            # get the time range for current clock entry
+            # set initial value for this candle
+            val = float('nan') if not fillgaps else last_val
+            # get the time range for current clock candle
             startdt = dtlist[i]
             enddt = None
+            # only set a end date if not last candle of clock
             if i < len(dtlist) - 1:
                 enddt = dtlist[i + 1]
+            # if there is no data to align, just set nan values
             if last_idx < 0:
-                res.append(aval)
+                res.append(val)
                 continue
+
+            # align data from slice to clock
             while True:
                 if (last_idx >= len(slicedata['float'])
                         or last_idx >= len(slicedata['value'])):
-                    # all consumed
+                    # all candles from data consumed
                     break
+                # current values from data
                 c_float = slicedata['float'][last_idx]
                 c_val = slicedata['value'][last_idx]
-                # forward until startdt is reached
+                # forward until start of clock candle is reached
                 if c_float < startdt:
                     if c_val == c_val:
                         last_val = c_val
                     last_idx += 1
                     continue
-                # wait, current value belongs to next clock entry
+                # current value belongs to next clock entry
                 if enddt and c_float >= enddt:
                     break
-                # set value
-                # either last value if nan and fillgaps or current value
-                if fillgaps and c_val != c_val:
-                    aval = last_val
+                # set value here
+                if fillgaps or c_val != c_val:
+                    val = last_val
                 elif c_val == c_val:
-                    aval = c_val
-                last_val = aval
+                    val = c_val
+                last_val = val
                 last_idx += 1
-            res.append(aval)
+
+            res.append(val)
         return res
+
+    """
+    def _get_data_from_list(self, llist, clk, fill_gaps=False):
+        '''
+        Generates data based on given clock
+        '''
+        data = []
+        c_idx = 0
+        sc = None
+        # go through all clock values and align given data to clock
+        for c in clk:
+            v = float('nan')
+            sc_prev = None
+            idx_prev = None
+            # go through all values to align starting from last index
+            for sc_idx in range(c_idx, len(llist)):
+                sc = self.clk[sc_idx]
+                if sc < c:
+                    # src clck is smaller: strat -> data_n
+                    if llist[sc_idx] == llist[sc_idx]:
+                        v = llist[sc_idx]
+                elif sc == c:
+                    # src clk equals: data_n -> data_n
+                    if v != v or llist[sc_idx] == llist[sc_idx]:
+                        v = llist[sc_idx]
+                    c_idx = sc_idx + 1
+                    break
+                elif sc > c:
+                    # scr clk bigger: data_m -> data_n
+                    if fill_gaps or (sc_prev and sc_prev < c):
+                        # fill only when clk value switched
+                        if sc_prev:
+                            v = llist[idx_prev]
+                            c_idx = idx_prev + 1
+                        else:
+                            v = llist[sc_idx]
+                            c_idx = sc_idx
+                    break
+                # remember current value if not nan
+                if llist[sc_idx] == llist[sc_idx]:
+                    sc_prev = sc
+                    idx_prev = sc_idx
+            # put value for c - clk pos from src clock
+            data.append(v)
+
+        return data
+    """
+
 
     def get_idx_for_dt(self, dt):
         return bisect_left(self._clk.array, bt.date2num(dt, tz=self._tz))
@@ -211,7 +263,9 @@ class DataClockHandler:
                 res['value'].append(float('nan'))
         return res
 
-    def get_data(self, obj, startidx=None, endidx=None, fillgaps=False):
+    def get_data(self, obj, startidx=None, endidx=None,
+                 fillgaps=False, fillnan=[]):
+
         slice_startdt = self.get_dt_at_idx(startidx)
         if endidx < len(self) - 1:
             slice_enddt = (
